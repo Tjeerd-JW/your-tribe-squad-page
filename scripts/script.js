acb603ccd9c3ea0f61e184d9880b6363963aa9a7
IsaacEswa <isaaceswa@gmail.com>
2025-09-17 11:02:06 +0200

    Locatie kaartjes toegevoegd
    ik heb de squad JS code gedupliceerd en aangepast naar locations

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,19 @@ import people from "../assets/people.json" with {type: "json"};
 import locations from "../assets/locations.json" with {type: "json"};
 
 // console.log(locations)
-console.log(people.people)
+// console.log(people.people)
 let squad = people.people;
+let locs = locations.locations;
+console.log(locations.locations);
+
 
 
 let cards = document.getElementById("card-holder");
+
+locationsCards();
 squadCards();
 
-
+// HIER BEGINT SQUAD FUNCTION --------------------------
 function squadCards() {
   let card = '';
   for (let x in squad) {
@@ -24,6 +29,29 @@ function squadCards() {
                 <img src="assets/photos/`+ squad[x].name + `.JPG" alt="" class="avatar">
                 <h2>`+ squad[x].name + `</h2>
                 <a href="`+ squad[x].link + `" target="_blank" class="card-link">Bekijk kaartje</a>
+              </div>
+        </div>
+      </div>
+    `
+  }
+  cards.innerHTML = card
+}
+
+
+
+// HIER BEGINT LOCATIONS FUNCTION --------------------------
+function locationsCards() {
+  let card = '';
+  for (let x in locs) {
+    card += `<div class="swiper-slide ">
+    <div class="card">
+        <div class="album-overlay">
+            <h2> <span>` + locs[x].location + `</span> </h2>
+          </div>
+          <img src="assets/locations/` + locs[x].image + `" alt="location" class="album-img">
+              <div class="card-info">       
+                <h2>`+ locs[x].name + `</h2>
+                <a href="`+ locs[x].link + `" target="_blank" class="card-link">Bekijk locatie</a>
               </div>
         </div>
       </div>
